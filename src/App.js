2c8a4303676762a919f91acf24cce4ad0b446585
Zachary Graham <zakgraham@gmail.com>
2019-08-19 15:51:37 -0700

    get X to click index per square

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+function checkForWinner(gameBoxes) {
+	console.log('gameboxes', gameBoxes)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [turn, changeTurn] = useState('X');
+	const [gameBoxes, updateGameBoxes] = useState(Array(9).fill(null));
+
+	const clickEndTurn = () => {
+		checkForWinner();
+	}
+
+	const checkForWinner = () => {
+		if(turn ==='X') {
+			console.log('turn ended?')
+		}
+	}
+
+	const clickBox = (index) => {
+		const newBoxData = gameBoxes.slice();
+		newBoxData[index] = 'X';
+		updateGameBoxes(newBoxData);
+	}
+
+	const renderBoxes = gameBoxes.map((square, index) => {
+		return <div className="box" key={index}>
+
+			<button
+				onClick={() => clickBox(index)}
+				className="game-button">
+				{square}
+			</button>
+
+		</div>
+	});
+
+	return (
+		<div className="App">
+			<div className="App-container">
+				<p>
+					Tic tac toe
+       			</p>
+				<p>
+					If you are {turn}, its your turn
+				</p>
+
+				{turn === 'X' ? 
+						<button onClick={() => changeTurn('O')} >Finish turn</button>
+							:
+						<button onClick={() => changeTurn('X')}>Finish turn</button>}
+
+
+				<br />
+				<div className="zg-game-container">
+					<div className="zg-game">
+						{renderBoxes}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
+
+
 
 export default App;
